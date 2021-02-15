fec1d7f3d6d8771d2bd13748abf1b7255b2f1c30
Joe Wickes <joe.c.wickes@gmail.com>
2021-02-15 13:56:41 -0500

    Update ssl

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://joe@localhost/date-ideas',
+  DATABASE_URL: `${process.env.DATABASE_URL}?ssl=true` || 'postgresql://joe@localhost/date-ideas',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://joe@localhost/date-ideas-test',
 }

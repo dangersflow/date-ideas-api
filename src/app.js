ebc1e997dc5561b05bf5d76c762aac880ceecf12
Joe Wickes <joe.c.wickes@gmail.com>
2021-02-17 09:28:49 -0500

    Remove unnecessary console log

--- src/app.js
+++ src/app.js
@@ -34,7 +34,6 @@ app.use('/api/excluded', excludedRouter)
 app.use(function errorHandler(error, req, res, next) {
   let response
   if (NODE_ENV === 'production') {
-    console.error(error)
     response = { error: { message: 'server error' } }
   } else {
     console.error(error)

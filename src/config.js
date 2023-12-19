a399de9265cdbe8409cf948b2e1a64a844e95f39
Frank <pacog879@hotmail.com>
2023-12-19 15:30:06 -0600

    modify env file

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'dev',
+  NODE_ENV: process.env.NODE_ENV || "dev",
   DATABASE_URL:
-    `${process.env.DATABASE_URL}` ||
-    'postgresql://joe@172.16.238.13:5432/date-ideas',
+    process.env.DATABASE_URL ||
+    "postgresql://joe:password@172.16.238.13:5432/date-ideas",
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
-    'postgresql://joe@172.16.238.13:5432/date-ideas-test',
+    "postgresql://joe:password@172.16.238.13:5432/date-ideas-test",
   DB_PORT: process.env.DB_PORT,
   HOST: process.env.HOST,
   USER: process.env.USER,

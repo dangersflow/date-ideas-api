2fce3f621e94289fd1e12f01f8093d5c190cd9fe
Frank <pacog879@hotmail.com>
2023-12-21 02:47:35 -0600

    update cors

--- src/app.js
+++ src/app.js
@@ -1,38 +1,48 @@
 // Dependencies
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
 
 // Routes
-const activitiesRouter = require('./routes/activities-router');
-const mealsRouter = require('./routes/meals-router');
-const dessertsRouter = require('./routes/desserts-router');
-const excludedRouter = require('./routes/excluded-router');
+const activitiesRouter = require("./routes/activities-router");
+const mealsRouter = require("./routes/meals-router");
+const dessertsRouter = require("./routes/desserts-router");
+const excludedRouter = require("./routes/excluded-router");
 
 const app = express();
 
-const morganOption = NODE_ENV === 'prod' ? 'tiny' : 'common';
+const morganOption = NODE_ENV === "prod" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.get('/api', (req, res) => {
-  res.send('Hello, Date Ideas!');
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.use('/api/activities', activitiesRouter);
-app.use('/api/meals', mealsRouter);
-app.use('/api/desserts', dessertsRouter);
-app.use('/api/excluded', excludedRouter);
+app.get("/api", (req, res) => {
+  res.send("Hello, Date Ideas!");
+});
+
+app.use("/api/activities", activitiesRouter);
+app.use("/api/meals", mealsRouter);
+app.use("/api/desserts", dessertsRouter);
+app.use("/api/excluded", excludedRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (NODE_ENV === 'prod') {
-    response = { error: { message: 'server error' } };
+  if (NODE_ENV === "prod") {
+    response = { error: { message: "server error" } };
   } else {
     console.error(error);
     response = { message: error.message, error };

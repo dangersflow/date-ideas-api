4796c7c1f29317ce92490e31d84c8111f66df6cd
Frank <pacog879@hotmail.com>
2023-12-21 02:57:57 -0600

    update cors

--- src/app.js
+++ src/app.js
@@ -24,6 +24,16 @@ app.use(
   })
 );
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/api", (req, res) => {
   res.send("Hello, Date Ideas!");
 });
